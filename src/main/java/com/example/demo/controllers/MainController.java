1f89d023f69404105939af21cfce85986f44788e
Vibs <vibej@hotmail.com>
2021-06-14 23:28:28 +0200

    lkhkjh

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-
-
+    // hej
     @GetMapping("/")
     public String index(){
         return "index";
